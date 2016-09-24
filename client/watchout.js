7b86cccbcba52f91b525d42c8e0656c8ff510e34
clarkdowner <cddowner@gmail.com>
2016-09-23 17:27:26 -0700

    Add additional circles. Code scrub.

--- client/watchout.js
+++ client/watchout.js
@@ -3,41 +3,26 @@ var svg = d3.select('svg'),
   height = +svg.attr( 'height');
 
 
-
 var update = function(values) {
 
-
-  values = d3.range(3).map(function() { return Math.random() * 720; });
-  var t = d3.transition()
-    .duration(1000);
+  values = d3.range(20).map(function() { return Math.random() * 720; });
 
   var circle = svg.selectAll('circle')
     .data(values);
 
-
-  // circle.style('fill', 'steelblue');
-  // circle.attr('r', 30);
-
-
+  // update
   circle.transition()
     .duration(1000)
     .attr('cx', function(d) { return randomCoOrdX(); })
     .attr('cy', function(d) { return randomCoOrdY(); });
-  
-  
-
-  // update
-  // circle.attr('cx', function(d) { return d; })
-
-  //   .transition(t);
-  //   myRand = randomCoOrd()
 
   // enter
   circle.enter().append('circle')
     .attr('cx', function(d) { return randomCoOrdX(); })
     .attr('cy', function(d) { return randomCoOrdY(); })
-    .attr('r', 30)
+    .attr('r', 10)
     .attr('style', 'fill:steelblue;');
+
   // exit
   circle.exit().remove();
 };
@@ -52,30 +37,5 @@ var randomCoOrdY = function() {
 };
 
 
-
-update([1, 2, 3]);
-
-
-
+update();
 setInterval(update, 1500);
-
-// -------------------------
-
-
-
-
-// (function() {
-
-//   function dance() {
-//   var circle = d3.selectAll('#circle-dance circle'),
-//     span = d3.selectAll('.circle-dance-x'),
-//     data = d3.range(3).map(function() { return Math.random() * 720; });
-
-//     circle.data(data).attr('cx', function(d) { return d; });
-//     span.data(data).text(function(d) { return d; });
-//   }
-
-//   dance();
-//   setInterval(dance, 2500);
-
-// })();
